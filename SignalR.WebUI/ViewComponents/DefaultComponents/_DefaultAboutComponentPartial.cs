54e0f89470e1dd22a6929d4fccf551eab8a49877
beko10 <berkay1905ber@gmail.com>
2024-05-25 00:28:35 +0300

    25.05.2024

---
+++ SignalR.WebUI/ViewComponents/DefaultComponents/_DefaultAboutComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace SignalR.WebUI.ViewComponents.DefaultComponents
+{
+    public class _DefaultAboutComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
